071843feb53ae8d7e5257dcd5bbd7abe968cfbd5
srk-bots <sakthisivam1610@gmail.com>
2025-07-26 21:58:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,7 +16,7 @@ video_mimetype = [
   "video/x-m4v",
   "video/quicktime",
   "video/mpeg"
-  ]
+]
 
 @app.on_message(filters.incoming & filters.command(['start', 'help']))
 def help_message(app, message):
@@ -25,12 +25,12 @@ def help_message(app, message):
 @app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
 def encode_video(app, message):
     if message.document:
-      if not message.document.mime_type in video_mimetype:
-        message.reply_text("```Invalid Video !\nMake sure its a valid video file.```", quote=True)
-        return
-    message.reply_text(" Added to queue...", quote=True)
+        if not message.document.mime_type in video_mimetype:
+            message.reply_text("```Invalid Video!\nMake sure it's a valid video file.```", quote=True, parse_mode="markdown")
+            return
+    message.reply_text("```Added to queue...```", quote=True, parse_mode="markdown")
     data.append(message)
     if len(data) == 1:
-      add_task(message)
+        add_task(message)
 
 app.run()
